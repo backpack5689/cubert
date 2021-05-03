73223cbad044b725b8f1d7a1f9ef07cdd6b70f91
backpack5689 <aaron.thompson1889@outlook.com>
2021-05-02 19:03:40 -0500

    Yes, there are bugs. No, I cannot fix them

--- frontend/src/app/stats/stats.component.ts
+++ frontend/src/app/stats/stats.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Chart, registerables } from 'chart.js';
+import { delay } from 'rxjs/operators';
 
 import { ApiService } from './../service/api.service';
 
@@ -32,41 +33,61 @@ export class StatsComponent implements OnInit {
           // Fetch all of the user's times
 		  this.apiService.getUserTimes(sessionStorage.getItem("_statId")).subscribe((data) => {
 			  this.userTimes = data;
-		  });
-          
-          // Generate graph x-axis labels
-          let labels = [];
-          for (let i = 1; i <= this.userTimes.length; i++) {
-              labels.push(i);
+      });
+        let attempts = 0;
+
+        //If there is an issue grabbing the users times, throw an error informing
+        //them that there was an issue trying to grab it.
+        while(this.userTimes.length == 0)
+        {
+          setTimeout(this.makeChart, 5000)
+          attempts++;
+          if(attempts == 25)
+          {
+            alert("Error: Your times were unable to load. Please try again later.");
+            break;
           }
-          
-          // Get actual times from this.userTimes
-          let times = [];
-          for (let i = 0; i < this.userTimes.length; i++) {
-              times.push(this.userTimes[i].time_scramble);
-          }
-          
-          // Set up chart
-          let data = {
-              labels: labels,
-              datasets: [{
-                label: "Times",
-                data: times,
-                fill: false,
-                borderColor: 'rgb(0, 0, 0)',
-                tension: 0.1
-              }]
-          };
-          
-          if (this.canvas !== undefined) {
-              console.log("Hello");
-              const chart: Chart = new Chart(this.canvas.nativeElement, {
-                  type: 'bar', data: data
-              });
-              
-              this.statChart = chart;
-          }
+        }
+        this.makeChart();
 	  }
+  }
+
+  makeChart(): void{
+    // Generate graph x-axis labels
+    let labels = [];
+    for (let i = 1; i <= this.userTimes.length; i++) {
+      labels.push(i);
+      }
+      
+      // Get actual times from this.userTimes
+      let times = [];
+      for (let i = 0; i < this.userTimes.length; i++) {
+        times.push(this.userTimes[i].time_completetime);
+      }
+      alert(times[0]);
+      alert(this.userTimes[0]);
+      
+      // Set up chart
+      let data = {
+        labels: labels,
+        datasets: [{
+          label: "Times",
+          data: times,
+          fill: false,
+          borderColor: 'rgb(0, 0, 0)',
+          backgroundColor: 'rgb(255, 0, 0)',
+          tension: 0.1
+        }]
+      };
+      
+      if (this.canvas !== undefined) {
+        console.log("Hello");
+        const chart: Chart = new Chart(this.canvas.nativeElement, {
+          type: 'bar', data: data
+        });
+        
+        this.statChart = chart;
+      }
   }
   
   ngOnDestroy(): void {
